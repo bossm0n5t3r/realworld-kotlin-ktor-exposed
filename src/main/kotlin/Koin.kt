379a06b86373c01d74f420cd600bfd1559e43d57
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-04-27 22:53:00 +0900

    fix: Rename to Koin.kt

--- src/main/kotlin/Koin.kt
+++ src/main/kotlin/Koin.kt
@@ -2,20 +2,18 @@ package me.bossm0n5t3r
 
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
+import me.bossm0n5t3r.configurations.DatabaseManager
+import me.bossm0n5t3r.configurations.DatabaseManagerImpl
 import org.koin.dsl.module
 import org.koin.ktor.plugin.Koin
 import org.koin.logger.slf4jLogger
 
-fun Application.configureFrameworks() {
+fun Application.configureKoin() {
     install(Koin) {
         slf4jLogger()
         modules(
             module {
-                single<HelloService> {
-                    HelloService {
-                        println(environment.log.info("Hello, World!"))
-                    }
-                }
+                single<DatabaseManager> { DatabaseManagerImpl() }
             },
         )
     }
