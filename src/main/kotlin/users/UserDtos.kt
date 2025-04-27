530a33623719a902882605f97bc8d955edecde7a
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-04-28 03:33:30 +0900

    feat: Add UserDtos.kt

---
+++ src/main/kotlin/users/UserDtos.kt
@@ -0,0 +1,40 @@
+package me.bossm0n5t3r.users
+
+data class UserDto(
+    val email: String,
+    val token: String,
+    val username: String,
+    val bio: String,
+    val image: String? = null,
+) {
+    constructor(userEntity: UserEntity) : this(
+        email = userEntity.email,
+        token = "",
+        username = userEntity.username,
+        bio = userEntity.bio,
+        image = userEntity.image,
+    )
+}
+
+data class CreateUserDto(
+    val username: String,
+    val email: String,
+    val password: String,
+)
+
+data class LoginUserDto(
+    val email: String,
+    val password: String,
+)
+
+data class UpdateUserDto(
+    val username: String? = null,
+    val email: String? = null,
+    val password: String? = null,
+    val bio: String? = null,
+    val image: String? = null,
+)
+
+data class UserWrapper<T>(
+    val user: T,
+)
