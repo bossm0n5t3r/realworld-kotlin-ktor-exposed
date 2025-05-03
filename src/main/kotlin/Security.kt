8b00fd8f9a51024440e398535e38c9081c95a5b8
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-05-03 22:31:00 +0900

    feat: Update Security.kt

--- src/main/kotlin/Security.kt
+++ src/main/kotlin/Security.kt
@@ -3,29 +3,35 @@ package me.bossm0n5t3r
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
 import io.ktor.server.application.Application
+import io.ktor.server.application.ApplicationCall
+import io.ktor.server.auth.UserIdPrincipal
 import io.ktor.server.auth.authentication
-import io.ktor.server.auth.jwt.JWTPrincipal
 import io.ktor.server.auth.jwt.jwt
+import io.ktor.server.auth.principal
+import me.bossm0n5t3r.configurations.LOGGER
+import me.bossm0n5t3r.securities.JwtProvider
+import me.bossm0n5t3r.securities.JwtProvider.ISSUER
+import me.bossm0n5t3r.securities.JwtProvider.toPublicKey
+import java.security.interfaces.ECPublicKey
 
 fun Application.configureSecurity() {
-    // Please read the jwt property from the config file if you are using EngineMain
-    val jwtAudience = "jwt-audience"
-    val jwtDomain = "https://jwt-provider-domain/"
-    val jwtRealm = "ktor sample app"
-    val jwtSecret = "secret"
+    val algorithm = Algorithm.ECDSA512(JwtProvider.hexEncodedPublicKey.toPublicKey() as ECPublicKey, null)
     authentication {
         jwt {
-            realm = jwtRealm
             verifier(
                 JWT
-                    .require(Algorithm.HMAC256(jwtSecret))
-                    .withAudience(jwtAudience)
-                    .withIssuer(jwtDomain)
+                    .require(algorithm)
+                    .withIssuer(ISSUER)
                     .build(),
             )
+            authSchemes("Token")
             validate { credential ->
-                if (credential.payload.audience.contains(jwtAudience)) JWTPrincipal(credential.payload) else null
+                val subject = credential.subject
+                LOGGER.info("Subject: $subject")
+                if (credential.payload.issuer.equals(ISSUER) && subject != null) UserIdPrincipal(subject) else null
             }
         }
     }
 }
+
+fun ApplicationCall.userId() = principal<UserIdPrincipal>()?.name ?: error("No user ID found")
