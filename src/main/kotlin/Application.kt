6d2090deb73d3fb16b67a1115bfeb0fd6375e796
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-05-03 22:37:00 +0900

    feat: Update Application.kt

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,18 +1,21 @@
 package me.bossm0n5t3r
 
 import io.ktor.server.application.Application
+import io.ktor.server.netty.EngineMain
+import me.bossm0n5t3r.configurations.DatabaseManager
+import org.koin.ktor.ext.inject
 
-fun main(args: Array<String>) {
-    io.ktor.server.netty.EngineMain
-        .main(args)
-}
+fun main(args: Array<String>) = EngineMain.main(args)
 
 fun Application.module() {
-    configureHTTP()
+    configureCORS()
     configureMonitoring()
-    configureFrameworks()
+    configureKoin()
+
+    val databaseManager: DatabaseManager by inject()
+    databaseManager.createTables()
+
     configureSerialization()
-    configureDatabases()
     configureSecurity()
     configureRouting()
 }
