f98cf49bc7cfe514b9823f81502173961d656e71
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-05-04 00:33:00 +0900

    feat: Add FollowingsRepository

---
+++ src/main/kotlin/profiles/FollowingsRepository.kt
@@ -0,0 +1,46 @@
+package me.bossm0n5t3r.profiles
+
+import me.bossm0n5t3r.configurations.DatabaseManager
+import org.jetbrains.exposed.sql.and
+import java.util.UUID
+
+class FollowingsRepository(
+    private val databaseManager: DatabaseManager,
+) {
+    suspend fun getAllFollowingsByUserId(userId: String) =
+        databaseManager.dbQuery {
+            FollowingEntity.find { Followings.userId eq UUID.fromString(userId) }.toList()
+        }
+
+    suspend fun isFollowing(
+        userId: String,
+        followerId: String,
+    ) = databaseManager.dbQuery {
+        FollowingEntity
+            .find {
+                (Followings.userId eq UUID.fromString(userId)) and (Followings.followerId eq UUID.fromString(followerId))
+            }.empty()
+            .not()
+    }
+
+    suspend fun addFollowing(
+        userId: String,
+        followerId: String,
+    ) = databaseManager.dbQuery {
+        FollowingEntity.new {
+            this.userId = UUID.fromString(userId)
+            this.followerId = UUID.fromString(followerId)
+        }
+    }
+
+    suspend fun deleteFollowing(
+        userId: String,
+        followerId: String,
+    ) = databaseManager.dbQuery {
+        FollowingEntity
+            .find {
+                (Followings.userId eq UUID.fromString(userId)) and (Followings.followerId eq UUID.fromString(followerId))
+            }.singleOrNull()
+            ?.delete()
+    }
+}
