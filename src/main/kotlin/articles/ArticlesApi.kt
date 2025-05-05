4eb448ee56c9834ccbed2e413f11b7f3349a6b9f
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-05-06 00:15:00 +0900

    feat: Update ArticlesApi.kt

--- src/main/kotlin/articles/ArticlesApi.kt
+++ src/main/kotlin/articles/ArticlesApi.kt
@@ -1,9 +1,14 @@
 package me.bossm0n5t3r.articles
 
 import io.ktor.server.auth.authenticate
+import io.ktor.server.request.receive
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
+import io.ktor.server.routing.delete
 import io.ktor.server.routing.get
+import io.ktor.server.routing.post
+import io.ktor.server.routing.put
+import me.bossm0n5t3r.userId
 import me.bossm0n5t3r.userIdOrNull
 
 fun Route.articlesApi(articlesService: ArticlesService) {
@@ -11,8 +16,45 @@ fun Route.articlesApi(articlesService: ArticlesService) {
         get("/articles") {
             val userId = call.userIdOrNull()
             val articleFilterDto = ArticleFilterDto(call.parameters)
-            val articles = articlesService.getAllArticles(userId, articleFilterDto)
-            call.respond(articles)
+            call.respond(articlesService.getAllArticles(userId, articleFilterDto))
+        }
+    }
+
+    get("/article/{slug}") {
+        val slug = call.parameters["slug"] ?: error("Invalid request")
+        call.respond(articlesService.getArticleBySlug(slug))
+    }
+
+    authenticate {
+        post("/articles") {
+            val userId = call.userId()
+            val createArticleDto = call.receive<ArticleWrapper<CreateArticleDto>>()
+            call.respond(articlesService.createArticle(userId, createArticleDto.article))
+        }
+
+        put("/articles/{slug}") {
+            val slug = call.parameters["slug"] ?: error("Invalid request")
+            val userId = call.userId()
+            val updateArticleDto = call.receive<ArticleWrapper<UpdateArticleDto>>()
+            call.respond(articlesService.updateArticle(slug, userId, updateArticleDto.article))
+        }
+
+        delete("/articles/{slug}") {
+            val slug = call.parameters["slug"] ?: error("Invalid request")
+            val userId = call.userId()
+            call.respond(articlesService.deleteArticle(slug, userId))
+        }
+
+        post("/articles/{slug}/favorite") {
+            val slug = call.parameters["slug"] ?: error("Invalid request")
+            val userId = call.userId()
+            call.respond(articlesService.favoriteArticle(slug, userId))
+        }
+
+        delete("/articles/{slug}/favorite") {
+            val slug = call.parameters["slug"] ?: error("Invalid request")
+            val userId = call.userId()
+            call.respond(articlesService.unfavoriteArticle(slug, userId))
         }
     }
 }
